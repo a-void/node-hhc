fa0a77ae609a87f5d284c174352d4265437cf3e7
Alexander Vitiuk <vitiuk.alex@gmail.com>
2014-07-11 18:11:40 -0400

    finally figured out routing problem

--- public/js/app.js
+++ public/js/app.js
@@ -2,25 +2,19 @@
 
 // Declare app level module which depends on filters, and services
 
-angular.module('hhc', [
-  'hhc.controllers',
-  'hhc.filters',
-  'hhc.services',
-  'hhc.directives'
-]).
-config(function ($routeProvider, $locationProvider) {
-  $routeProvider.
-    when('/view1', {
-      templateUrl: 'partials/partial1',
-      controller: 'MyCtrl1'
-    }).
-    when('/view2', {
-      templateUrl: 'partials/partial2',
-      controller: 'MyCtrl2'
-    }).
-    otherwise({
-      redirectTo: '/view1'
-    });
-
-  $locationProvider.html5Mode(true);
-});
+angular.module('hhc', ['hhc.filters', 'hhc.services', 'hhc.directives']).
+    config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
+        $routeProvider.
+            when('/', {
+                templateUrl: 'partials/index',
+                controller: 'index'
+            }).
+            when('/hero', {
+                templateUrl: 'partials/hero-list',
+                controller: 'hero'
+            }).
+            otherwise({
+                redirectTo: '/'
+            });
+        $locationProvider.html5Mode(true);
+    }]);
